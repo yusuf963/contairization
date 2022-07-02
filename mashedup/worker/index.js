ed1afb102553c00f50314d789825bb678c9cf473
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-02 19:04:03 +0100

    add node_module to gitignore files and install packages

--- mashedup/worker/index.js
+++ mashedup/worker/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! from Worker');
 })
 
 PORT = process.env.PORT || 5002;

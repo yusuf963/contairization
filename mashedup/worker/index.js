1c1cf5975b76223bdedf031374e5c2d22979c7e6
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-03 11:31:26 +0100

    add logic for worker

--- mashedup/worker/index.js
+++ mashedup/worker/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const redis = require('redis')
+const keys = require('./keys')
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World! from Worker');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 })
+const sub = redisClient.duplicate()
+
+const fib = (index) => {
+  if (index < 2) return 1
+  return fib(index - 1) + fib(index - 2)
+
+}
+
+console.log(fib(4))
 
 PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
-    console.log(`'Worker app listening on port ${PORT}!'`);
+  console.log(`'Worker app listening on port ${PORT}!'`);
 })
